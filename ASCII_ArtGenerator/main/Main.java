32254ad2c6107319d7db330c1827c6e55abf832e
maparker77 <maparker3@wisc.edu>
2020-11-06 22:02:39 -0600

    ASCII gray scale string added

--- ASCII_ArtGenerator/main/Main.java
+++ ASCII_ArtGenerator/main/Main.java
@@ -1,6 +1,8 @@
 package main;
 
 public class Main {
+	
+	private static String ASCIIGrayScale = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'. "; 
 
   public static void main(String[] args) {
     // TODO Auto-generated method stub
