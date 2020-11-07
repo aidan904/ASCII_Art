f82e5c44c4e664ddbdc746dad8b7beb24bff7d9b
maparker77 <maparker3@wisc.edu>
2020-11-06 22:21:21 -0600

    Loads an image

--- ASCII_ArtGenerator/main/Main.java
+++ ASCII_ArtGenerator/main/Main.java
@@ -1,12 +1,26 @@
 package main;
 
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+import javax.imageio.ImageIO;
+
 public class Main {
-	
-	private static String ASCIIGrayScale = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'. "; 
 
-  public static void main(String[] args) {
-    // TODO Auto-generated method stub
+    private static final String ASCIIGrayScale = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'. ";
 
-  }
+    public static void main(String[] args) {
+	Scanner scanner = new Scanner(System.in);
+	String fileName = scanner.nextLine();
+	Image img;
+	try {
+	    img = ImageIO.read(new File(fileName));
+	    System.out.println("Image loaded");
+	} catch (IOException e) {
+	    System.out.println("Lol fuck you didnt work");
+	}
+    }
 
 }
